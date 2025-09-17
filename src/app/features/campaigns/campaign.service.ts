dc6a95ed8fe47afc0aafa4b6f67ec022890906c3
kevit-yagnik-vora <yagnik.vora@kevit.io>
2025-09-17 16:55:08 +0530

    UI changes and database schema change

--- src/app/features/campaigns/campaign.service.ts
+++ src/app/features/campaigns/campaign.service.ts
@@ -15,9 +15,9 @@ export class CampaignService {
     );
   }
 
-  getCampaignById(id: string) {
-    return this.http.get<any>(`${this.base}/${id}`);
-  }
+  // getCampaignById(id: string) {
+  //   return this.http.get<any>(`${this.base}/${id}`);
+  // }
 
   get(id: string) {
     return this.http.get(`${this.base}/${id}`);
@@ -70,12 +70,22 @@ export class CampaignService {
   getCampaignDetails(id: string) {
     return this.http.get<any>(`${this.base}/${id}/details`);
   }
-
-  getCampaignMessages(campaignId: string): Observable<any[]> {
-    return this.http.get<any[]>(
-      `${this.base}/${campaignId}/messages`
-    );
+  getCampaignMessages(campaignId: string) {
+    return this.http.get<any>(`${this.base}/${campaignId}/messages`);
   }
+
+  launchCampaign(campaignId: string, userId?: string) {
+    return this.http.post<any>(`${this.base}/${campaignId}/launch`, { userId });
+  }
+
+  // (Optional) poll campaign status
+  getCampaignById(campaignId: string) {
+    return this.http.get<any>(`${this.base}/${campaignId}`);
+  }
+
+  // getCampaignMessages(campaignId: string): Observable<any[]> {
+  //   return this.http.get<any[]>(`${this.base}/${campaignId}/messages`);
+  // }
 
   // backend endpoint should accept workspaceId and comma separated tags e.g.
   // GET /contacts/count?workspaceId=...&tags=tag1,tag2
--- src/app/features/campaigns/pages/details/details.component.ts
+++ src/app/features/campaigns/pages/details/details.component.ts
@@ -2,67 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CampaignService } from '../../campaign.service';
 import { ContactService } from '../../../contacts/contact.service';
+import { interval, Subject, switchMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-campaign-details',
   templateUrl: './details.component.html',
 })
 export class CampaignDetailsComponent implements OnInit {
-  campaign: any;
-  contacts: any[] = [];
-  loading = true;
+  campaignId!: string;
+  campaignMessages: any;
 
   constructor(
     private route: ActivatedRoute,
-    private campaignService: CampaignService,
-    private contactService: ContactService
+    private campaignService: CampaignService
   ) {}
 
-  messages: any[] = [];
-
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.campaignService
-        .getCampaignById(id)
-        .subscribe((c) => (this.campaign = c));
-      this.campaignService
-        .getCampaignMessages(id)
-        .subscribe((m) => (this.messages = m));
-    }
+    this.campaignId = this.route.snapshot.paramMap.get('id')!;
+    this.fetchCampaignMessages();
   }
 
-  // private loadCampaign(campaignId: string) {
-  //   this.campaignService.getCampaignById(campaignId).subscribe({
-  //     next: (data) => {
-  //       this.campaign = data;
-  //       this.loading = false;
-
-  //       // fetch contacts by tags
-  //       if (this.campaign?.tags?.length) {
-  //         this.contactService
-  //           .getContactsByTags(this.campaign.workspaceId, this.campaign.tags)
-  //           .subscribe((res) => (this.contacts = res));
-  //       }
-  //     },
-  //     error: (err) => {
-  //       console.error('Error loading campaign', err);
-  //       this.loading = false;
-  //     },
-  //   });
-  // }
-  private loadCampaign(campaignId: string) {
-    this.campaignService.getCampaignDetails(campaignId).subscribe({
-      next: (data) => {
-        console.log('Campaign details', data);
-        this.campaign = data.campaign;
-        this.contacts = data.targetedContacts;
-        this.loading = false;
+  fetchCampaignMessages() {
+    this.campaignService.getCampaignMessages(this.campaignId).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.campaignMessages = res;
       },
-      error: (err) => {
-        console.error('Error loading campaign details', err);
-        this.loading = false;
-      },
+      error: (err) => console.error(err),
     });
   }
 }
